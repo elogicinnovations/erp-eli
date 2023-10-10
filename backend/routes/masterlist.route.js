4daef51599e7122fa7cbb28d31c25b9aaed41cf2
Chscodes <chsdcode@gmail.com>
2023-10-10 10:43:01 +0800

    Second Commit

--- backend/routes/masterlist.route.js
+++ backend/routes/masterlist.route.js
@@ -38,6 +38,70 @@ router.route("/login").post(async (req, res) => {
 })
 
 
+//CREATE
+// router.route('/add').post(async (req, res) => {
+//     const b = req.body
+//     await User.create(b).then((add) => {
+//         if(add)
+//         {
+//             res.json({status : true})
+//         }
+//         else
+//         {
+//             res.status(400).json({status: false})
+//         }
+//     }).catch((err) => {
+//         console.error(err)
+//         res.status(400)
+//     });
+// });
+
+
+// //UPDATE
+// router.route('/update').put(async (req, res) => {
+//     await User.update(req.body,{
+//         where: {
+//             id : req.query.id
+//         }
+//     }).then((update) => {
+//         if(update) {
+//             res.json({success:true})
+//         }
+//         else{
+//             res.status(400).json({success:false})
+//         }
+//     }).catch((err) => {
+//         console.error(err)
+//         res.status(409).json({errorMessage:err})
+//     });
+// });
+
+
+// //DELETE:
+// router.route('/delete').delete(async (req, res) => {
+//     const b = req.query.id
+//     await User.destroy({
+//         where : {
+//             id: b
+//         }
+//     }).then(
+//         (del) => {
+//             if(del){
+//                 res.json({success : true})
+//             }
+//             else{
+//                 res.status(400).json({success : false})
+//             }
+//         }
+//     ).catch(
+//         (err) => {
+//             console.error(err)
+//             res.status(409)
+//         }
+//     );
+// });
+
+
 
 
 module.exports = router;
