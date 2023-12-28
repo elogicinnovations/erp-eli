27c84a05a0d6284fbc7f20f745a21245d6478752
SlashJoseph <jjumaday@slashtechsolutions.com>
2023-12-28 17:05:03 +0800

    Joseph wfh task 12-28

--- frontend/src/modules/Administrator/sub-modules/UserMasterData/Product Management/ProductList/ProductSupplier.jsx
+++ frontend/src/modules/Administrator/sub-modules/UserMasterData/Product Management/ProductList/ProductSupplier.jsx
@@ -577,7 +577,6 @@ function ProductSupplier() {
                           <tr>
                             <th>Supplier Code</th>
                             <th>Sub-Part Name</th>
-                            <th>Supplier Name</th>
                             <th>Description</th>
                           </tr>
                         </thead>
@@ -586,7 +585,6 @@ function ProductSupplier() {
                             <tr>
                               <td>{subpart.subPart.subPart_code}</td>
                               <td>{subpart.subPart.subPart_name}</td>
-                              <td>{subpart.subPart.supplier}</td>
                               <td>{subpart.subPart.subPart_desc}</td>
                             </tr>
                           ))}
