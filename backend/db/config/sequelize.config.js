527db993b90b1495fe7d0a1fee94236391bb877d
Chscodes <chsdcode@gmail.com>
2024-05-03 17:25:43 +0800

    seq

--- backend/db/config/sequelize.config.js
+++ backend/db/config/sequelize.config.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize({
   database: "erp_sbf",
   dialect: "mysql",
   username: "root",
-  password: "",
+  // password: "",
   password: "LIOyqn49725",
   timezone: "+08:00", // Asia/Manila timezone
   alter: false,
