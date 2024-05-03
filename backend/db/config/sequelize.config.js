9077c9c22bd952a9e90443b354d8dd43fe3a7cdf
JosephCode14 <josephjumaday14@gmail.com>
2024-05-03 17:20:08 +0800

    Before UAT

--- backend/db/config/sequelize.config.js
+++ backend/db/config/sequelize.config.js
@@ -1,14 +1,14 @@
 const { Sequelize, Op } = require("sequelize");
 
 const sequelize = new Sequelize({
-  host: "localhost",
-  // host: "180.232.110.228",
+  // host: "localhost",
+  host: "180.232.110.228",
   database: "erp_sbf",
   dialect: "mysql",
   username: "root",
   password: "",
-  // password: "LIOyqn49725",
-  // timezone: "+08:00", // Asia/Manila timezone
+  password: "LIOyqn49725",
+  timezone: "+08:00", // Asia/Manila timezone
   alter: false,
 });
 
--- frontend/src/assets/global/url.js
+++ frontend/src/assets/global/url.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:8083';
-// const BASE_URL = 'http://180.232.110.228:8083';
+// const BASE_URL = 'http://localhost:8083';
+const BASE_URL = 'http://180.232.110.228:8083';
 export default BASE_URL;
