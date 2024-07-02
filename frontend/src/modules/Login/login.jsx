96b41dca5ae1e20d099c28bf85d114715ea23651
Chscodes <chsdcode@gmail.com>
2024-07-02 17:29:16 +0800

    port env 80 two2

--- frontend/src/modules/Login/login.jsx
+++ frontend/src/modules/Login/login.jsx
@@ -146,7 +146,7 @@ const Login = () => {
               <img src={SBFLOGO} alt="" srcset="" />
             </div>
             <div className="logintextaccount">
-              Test (3000) Login to your account
+              Test (80 env) Login to your account
             </div>
           </div>
 
